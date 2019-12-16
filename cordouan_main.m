92d614dada9aa19b9f35951a418ca751419c0eb8
uri <utayvah@gmail.com>
2019-12-16 16:52:26 -0500

    add discretization

--- cordouan_main.m
+++ cordouan_main.m
@@ -1,24 +1,30 @@
 function [n] = cordouan_main(input_file_name, t_smp, A_smp, t_ref, A_ref)
+%% error checking
+assert(t_smp(1) == t_ref(1), 'Time domain traces must start at the same time');
+
 %% load data
 input = load_input(input_file_name);
 
+input.settings.fft
 %% calculate experimental transfer function
-[freq_smp spec_smp] = fft_func(t_smp, A_smp, input.settings.fft);
-[freq_smp spec_ref] = fft_func(t_ref, A_ref, input.settings.fft);
-
-range = 
-
-tf_exp = spec_smp./spec_ref;
+[freq_smp, spec_smp] = fft_func(t_smp, A_smp, input.settings.fft);
+[freq_ref, spec_ref] = fft_func(t_ref, A_ref, input.settings.fft);
 
 
-%% build transfer function 
-transferFunction = build_transfer_function( %layer and interface specification,...
-                                     %experimental transfer function )
+%% discretize (maybe put in its own function)
+freq = input.settings.freq_lo:input.settings.freq_step:input.settings.freq_hi;
+spec_smp_disc = interp1(freq_smp, spec_smp, freq);
+spec_ref_disc = interp1(freq_ref, spec_ref, freq);
 
-%% loop over data points
 
-for % freq in frequency range
-    %something like:
-    n(freq) = fmincom(@(n) transferFunction(n, freq), ...)
-end
+% %% build transfer function 
+% transferFunction = build_transfer_function( %layer and interface specification,...
+%                                      %experimental transfer function )
+% 
+% %% loop over data points
+% 
+% for % freq in frequency range
+%     %something like:
+%     n(freq) = fmincom(@(n) transferFunction(n, freq), ...)
+% end
 end
--- fft_func.m
+++ fft_func.m
@@ -3,6 +3,6 @@
 function [freq, spec] = fft_func(time, amplitude, options)
 amplitude_windowed = TD_window(time, amplitude, ...
     options.windowing_type, options.windowing_width);
-freq = faxis(time, options.padding);
-spec = fft(amplitude_windowed, options.padding);
+freq = faxis(time, 2^options.padding);
+spec = fft(amplitude_windowed, 2^options.padding);
 end
