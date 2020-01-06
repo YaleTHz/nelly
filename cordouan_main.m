902ddc0b026f552297b120cc6d95f72e5655f14d
uri <utayvah@gmail.com>
2020-01-06 16:27:54 -0500

    get cordouan_main working

--- cordouan_main.m
+++ cordouan_main.m
@@ -1,8 +1,7 @@
-%function [n] = cordouan_main(input_file_name, t_smp, A_smp, t_ref, A_ref)
+function [n_fit] = cordouan_main(input_file_name, t_smp, A_smp, t_ref, A_ref)
 %% error checking
-%assert(t_smp(1) == t_ref(1), 'Time domain traces must start at the same time');
 
-%% load data
+%% load and process data
 input = load_input(input_file_name);
 
 %% check time ranges, and pad as necessary
@@ -10,8 +9,8 @@ input = load_input(input_file_name);
 
 %% calculate experimental transfer function
 % fourier transform time domain data
-[freq_smp, spec_smp] = fft_func(t_smp, A_smp, input.settings.fft);
-[freq_ref, spec_ref] = fft_func(t_ref, A_ref, input.settings.fft);
+[freq_smp, spec_smp] = fft_func(t, A_smp_pad, input.settings.fft);
+[freq_ref, spec_ref] = fft_func(t, A_ref_pad, input.settings.fft);
 
 % discretize
 freq = input.settings.freq_lo:input.settings.freq_step:input.settings.freq_hi;
@@ -22,15 +21,19 @@ spec_ref_disc = interp1(freq_ref, spec_ref, freq);
 tf_spec = spec_smp_disc./spec_ref_disc;
 
 %% build transfer function 
-func = build_transfer_function(input);
+[func, ~] = build_transfer_function(input);
 
-%f(w, n_solve)
-func(0.2, 4)
-% 
-% %% loop over data points
-% 
-% for % freq in frequency range
-%     %something like:
-%     n(freq) = fmincom(@(n) transferFunction(n, freq), ...)
-% end
+%% perform fitting
+n_fit = zeros(2, numel(freq));
+n_prev = [5 0];
+
+for ii = 1:numel(freq)
+    err = @(n) abs(func(freq(ii), complex(n(1), n(2)))-tf_spec(ii));
+    opts = optimset('PlotFcns', @optimplotfval);
+    n_opt = fminsearch(err, n_prev, opts);
+    n_prev = n_opt;
+    n_fit(:,ii) = n_opt;
+    fprintf('%0.2f THz: n = %s\n', freq(ii), num2str(complex(n_opt(1), n_opt(2))))
+end
+
 end
