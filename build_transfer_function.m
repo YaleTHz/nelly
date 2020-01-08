0cb2ef59ff425eabe0236ec464dc1ab04626bf79
uri <utayvah@gmail.com>
2020-01-08 11:04:44 -0500

    add fabry perot test (single layer)

--- build_transfer_function.m
+++ build_transfer_function.m
@@ -44,25 +44,22 @@ func = @(freq, n_solve) tran(freq, n_solve)*...
             n2 = geom{ind+1}.n_func(freq, n_solve);
             d =  geom{ind  }.d;
             fp_single = fp(freq, d, n0, n1, n2);
-            
+                        
             % determine number of reflections 
             % maybe change this to get parameters (t_cut, a_cut) from
             % input? Or automatically read get t_cut from input?
             
-            % based on time: consider reflections until they would be 
+            % based on time: include reflections until they would be 
             % separated from the main pulse by t_cut picoseconds
-            t_refl = 1e12*2*d*n1/c; t_cut = 5;
+            t_refl = 1e12*2*d*n1/c; t_cut = 20;
             m_time = floor(t_cut/t_refl);
-            %fprintf('d = %0.2f, n1 = %0.2f \n\t=> %0.2f ps per reflection \n\t=> m = %d\n',...
-            %    d, n1, t_refl, m_time)
             
-            % based on amplitude: consider reflections until their
+            % based on amplitude: include reflections until their
             % amplitude if neglible.
-            a_cut = 0.01;
+            a_cut = 1e-5;
             m_amp = round(log(a_cut)/log(abs(fp_single)));
-            %fprintf('fp = %s per reflection => m = %d\n',...
-            %    num2str(abs(fp_single)), m_amp)
             
+            min([m_time m_amp]);
             m = 0:min([m_time m_amp]);
             coeff = coeff*sum(fp_single.^m);
         end
