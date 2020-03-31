c06473ead67ee5ad648e713deace771c426138c2
uri <utayvah@gmail.com>
2020-03-31 15:21:06 -0400

    ready to merge

--- exp_tf.m
+++ exp_tf.m
@@ -3,8 +3,8 @@ function [freq, tf_spec, freq_full, tf_full, spec_smp, spec_ref] = exp_tf(t_smp,
 [t, A_smp_pad, A_ref_pad] = time_pad(t_smp, A_smp, t_ref, A_ref);
 
 %remove offset to avoid artificial drop upon zero padding
-%A_smp_pad = A_smp_pad - A_smp_pad(end);
-%A_ref_pad = A_ref_pad - A_ref_pad(end);
+A_smp_pad = A_smp_pad - A_smp_pad(end);
+A_ref_pad = A_ref_pad - A_ref_pad(end);
 
 
 %% calculate experimental transfer function
